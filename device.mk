962ce3e48eb37e608bbc87e961f086532c11789e
nzlnice <3193782263@qq.com>
2024-01-30 19:21:22 +0800

    change

--- device.mk
+++ device.mk
@@ -1,44 +1,12 @@
 #
-# Copyright (C) 2020 The TwrpBuilder Open-Source Project
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 LOCAL_PATH := device/vivo/PD1936
 
-PRODUCT_PACKAGES += \    
-    android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    
-
-PRODUCT_HOST_PACKAGES += \
-    libandroidicu
-
-# SHIPPING API
-PRODUCT_SHIPPING_API_LEVEL := 29
-# VNDK API
-PRODUCT_TARGET_VNDK_VERSION := 30
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
-
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
 # otacert
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     $(DEVICE_PATH)/security/vivo_releasekey
-
-# PRODUCT_RELEASE_NAME ro.twrp.device.name
-PRODUCT_PROPERTY_OVERRIDES += ro.twrp.device.name=$(PRODUCT_RELEASE_NAME)
